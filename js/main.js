42a84a853ef1e4be19001bf454e2eade2562bbfa
kushkoalexgit config --global user.email kushko.alex@gmail.com <kushko.alex@gmail.com>
2014-07-03 23:26:36 +0300

    -

--- js/main.js
+++ js/main.js
@@ -3,8 +3,17 @@ $(function () {
 
     $(".block").click(function () {
         $(".intro-container").fadeOut(1000, function () {
-            $(".imagine").fadeIn(1000);
+            $(".imagine").fadeIn(1000, function () {
+                setTimeout(function () {
+                    location.href = 'main.html';
+                }, 2000);
+            });
         });
     });
 
 });
+
+
+// http://stackoverflow.com/questions/5002351/jquery-fade-css-background-image-change
+
+ //    http://stackoverflow.com/questions/4630947/how-do-i-change-the-background-image-using-jquery-animation
