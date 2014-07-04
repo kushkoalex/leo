b76fd8a2d6a91d5c80e5310424e3ff5cab5ba02f
Alexander Kushko <kushko.alex@gmail.com>
2014-07-04 13:56:54 +0300

    --

--- js/init.js
+++ js/init.js
@@ -9,26 +9,30 @@ $(function () {
         });
     });
 
-    var images = ['img/bg.png', 'img/bg2.png'];
+    console.log(LEO.settings.specialContent);
+
     var i = 0;
     var delay = 5000;
 
 
     var changeImage = function () {
         $("#bg-layer").animate({opacity: 0}, 'slow', function () {
-            if (i == images.length) {
+            if (i == LEO.settings.specialContent.items.length) {
                 i = 0;
             }
-//            console.log(i);
-            var image = images[i];
-            var bg = 'url(' + image + ')';
+            console.log(i);
+            var item = LEO.settings.specialContent.items[i];
+            console.log(item);
+            var bg = 'url(' + LEO.settings.specialContent.imagePath+ item.imageFileName + ')';
             i++;
             $(this).css({'background-image': bg }).animate({opacity: 1});
+            $("#title").html(item.title);
+            $("#text").html(item.text);
         });
     };
 
-//    changeImage();
-//    setInterval(changeImage, delay);
+    changeImage();
+    setInterval(changeImage, delay);
 
     $("#contentFrameWrapper").draggable(
         {
